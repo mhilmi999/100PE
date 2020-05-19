2782ad6f1ac63fa6bb0f95b6cb48a5a8a0ac1914
mhilmi999 <54462717+mhilmi999@users.noreply.github.com>
2020-05-19 21:08:58 +0700

    Update 21-30.c

--- 21-30.c
+++ 21-30.c
@@ -13,34 +13,45 @@ Output :
 Jika “+” atau “-“ output hasil pertambahan atau pengurangan dari a dan
 b, jika “>” atau “<” atau “=” output “benar” jika kondisi memenuhi,
 output salah jika kondisi tidak memenuhi.
-#include<stdio.h>
-//author : Evelyn Tjitrodjojo
-int main(){
-int a,c;
-char b;
-while(scanf("%d %c %d",&a,&b,&c)!=EOF){ //input hingga EOF
-if(b=='-') printf("%d\n",a-c); //operasi pengurangan
-else if(b=='+') printf("%d\n",a+c); //operasi pertambahan
-else if(b=='*') printf("%d\n",a*c); //operasi perkalian
-else if(b=='<') { //cek apakah a<b
-if (a<c){
-printf("benar\n");
+#include<stdio.h> 
+//author : Evelyn Tjitrodjojo 
+int main()
+{ 
+  int a,c; 
+  char b; 
+  while(scanf("%d %c %d",&a,&b,&c)!=EOF)
+  { //input hingga EOF 
+   if(b=='-') printf("%d\n",a-c); //operasi pengurangan 
+   else if(b=='+') printf("%d\n",a+c); //operasi pertambahan 
+   else if(b=='*') printf("%d\n",a*c); //operasi perkalian 
+   else if(b=='<')
+   { //cek apakah a<b 
+    if (a<c)
+    { 
+     printf("benar\n"); 
+    } 
+    else printf("salah\n"); 
+   } 
+   else if(b=='>') 
+   { //cek apakah b>c 
+    if (a>c)
+    { 
+     printf("benar\n"); 
+    } 
+    else printf("salah\n"); 
+   } 
+   else if(b=='=') 
+   { //cek apakah a=c 
+    if (a==c)
+    { 
+     printf("benar\n"); 
+    } 
+    else printf("salah\n"); 
+   } 
+  } 
+  return 0; 
 }
-else printf("salah\n");
-}
-else if(b=='>') { //cek apakah b>c
-if (a>c){
-printf("benar\n");
-}
-else printf("salah\n");
-}
-else if(b=='=') { //cek apakah a=c
-if (a==c){
-printf("benar\n");
-}
-
-else printf("salah\n");
-}
+ 
 
 
 
