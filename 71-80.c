f5c7b4744a175abe722e78806dccdffcb9a8aefd
mhilmi999 <54462717+mhilmi999@users.noreply.github.com>
2020-05-20 10:46:52 +0700

    Update 71-80.c

--- 71-80.c
+++ 71-80.c
@@ -1,11 +1,19 @@
 71. CPTTR4
-• Source Code
-  • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul CPTTR4 dimana user diberi 5 macam input yang terdiri dari t yaitu banyak test case, dan untuk tiap test case akan diberi l yaitu banyak baris, c banyak kolom, h tinggi persegi, dan w lebar persegi. Nanti untuk tiap test case, user harus mengoutputkan sebuah pola dimana tiap pola itu terdiri dari persegi yang bertumpuk-tumpuk, dimana banyak baris dan banyak kolom berarti berapa banyak persegi yang harus dioutputkan dan persegi tersebut harus memenuhi pola yang diminta SPOJ. Untuk melihat pola tersebut bisa langsung melihat di SPOJ agar lebih jelas.
+
+ • Penjelasan
+Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul CPTTR4 dimana user diberi 5 
+  macam input yang terdiri dari t yaitu banyak test case, dan untuk tiap test case akan diberi l yaitu banyak baris, 
+c banyak kolom, h tinggi persegi, dan w lebar persegi. Nanti untuk tiap test case, user harus mengoutputkan 
+sebuah pola dimana tiap pola itu terdiri dari persegi yang bertumpuk-tumpuk, dimana banyak baris dan banyak kolom 
+berarti berapa banyak persegi yang harus dioutputkan dan persegi tersebut harus memenuhi pola yang diminta SPOJ. 
+Untuk melihat pola tersebut bisa langsung melihat di SPOJ agar lebih jelas.
+
 • Input
 t = banyak test case, l = banyak baris, c = banyak kolom, h = tinggi persegi, w = lebar persegi
+
 • Output
 Pola yang diminta oleh SPOJ
+
 Source Code:
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -39,13 +47,18 @@ int main(){
 
 72. TWINS
 • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal vJudge yang berjudul TWINS dimana user diminta untuk membuat sebuah Menara kembar dengan tinggi yang sama. User akan diberi Menara dengan tinggi bermacam-macam dan diminta untuk menentukan apakah memungkinkan untuk membuat Menara kembar tersebut (contoh
+Program ini adalah program yang dibuat untuk menyelesaikan soal vJudge yang berjudul TWINS dimana user 
+diminta untuk membuat sebuah Menara kembar dengan tinggi yang sama. User akan diberi Menara dengan tinggi 
+bermacam-macam dan diminta untuk menentukan apakah memungkinkan untuk membuat Menara kembar tersebut (contoh
+semisal mereka diberi 4 menara dengan tinggi masing-masing 4, 2, 2, dan 3. Maka mereka akan bisa membuat 2 menara 
+dengan tinggi 4 yaitu mengambil Menara tinggi 4 dan satunya 2+2, 3 tidak perlu dipakai)
 
-semisal mereka diberi 4 menara dengan tinggi masing-masing 4, 2, 2, dan 3. Maka mereka akan bisa membuat 2 menara dengan tinggi 4 yaitu mengambil Menara tinggi 4 dan satunya 2+2, 3 tidak perlu dipakai)
 • Input
 t = banyak test case, n = banyak Menara yang diberikan, h tiap n = tinggi Menara tersebut
+
 • Output
 Bisa membuat Menara kembar tersebut atau tidak
+
 • Source Code
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -89,12 +102,19 @@ return 0;
 
 
 73. HIGH CG BOY • Source Code
- • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul HIGH CG BOY dimana user diminta untuk mengoutputkan IPK final dari suatu mahasiswa yang kuliah selama 8 semester. Disini diasumsikan bahwa dari semester ke n sampai ke semester 8, mahasiswa tersebut akan selalu mendapatkan IPK 4. User akan diberikan input tc atau banyak test case, n adalah semester mahasiswa tersebut saat ini, dan untuk tiap n, akan ada nilai IPK mahasiswa tersebut dari 1 sampai ke n.
+• Penjelasan
+Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul HIGH CG BOY dimana user 
+diminta untuk mengoutputkan IPK final dari suatu mahasiswa yang kuliah selama 8 semester.
+Disini diasumsikan bahwa dari semester ke n sampai ke semester 8, mahasiswa tersebut akan selalu mendapatkan IPK 4. 
+User akan diberikan input tc atau banyak test case, n adalah semester mahasiswa tersebut saat ini, dan untuk tiap n, 
+akan ada nilai IPK mahasiswa tersebut dari 1 sampai ke n.
+
 • Input
 t = banyak test case, n = semester mahasiswa tersebut sekarang, x tiap n = nilai mahasiswa tersebut di semester sebelumnya
+
 • Output
 IPK final mahasiswa tersebut
+
 Source Code:
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -119,12 +139,19 @@ int main(){
 
 
 74. CODER OR NONCODER • Source Code
-  • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul CODER OR NONCODER dimana user diminta untuk mengoutputkan persentase kemungkinan seorang mahasiswa untuk bisa menebak (mahasiswa ini punya kemampuan khusus) bahwa seseorang adalah seorang programmer di suatu universitas. User akan diberikan input berupa tc atau test case, x yang mana adalah persentase banyak programmer di dalam universitas tersebut, dan y adalah persentase kemungkinan mahasiswa tersebut bisa menebak dengan benar
+• Penjelasan
+Program ini adalah program yang dibuat untuk menyelesaikan soal SPOJ yang berjudul CODER OR NONCODER dimana user diminta 
+untuk mengoutputkan persentase kemungkinan seorang mahasiswa untuk bisa menebak (mahasiswa ini punya kemampuan khusus) 
+  bahwa seseorang adalah seorang programmer di suatu universitas. User akan diberikan input berupa tc atau test case, 
+x yang mana adalah persentase banyak programmer di dalam universitas tersebut, dan y adalah persentase kemungkinan mahasiswa 
+tersebut bisa menebak dengan benar
+
 • Input
 t = banyak test case, x = persentase banyak programmer, y = kemungkinan mahasiswa tersebut bisa menebak dengan benar
+
 • Output
 Persentase kemungkinan dia bisa menebak dengan benar semua mahasiswa di universitas tersebut
+
 Source Code:
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -148,11 +175,19 @@ int main(){
 
 75. MAGNETS
 • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal Codeforces yang berjudul MAGNETS dimana user ditugaskan membantu untuk menentukan berapa banyak grup magnet yang akan terbentuk dari suatu susunan magnet. Cara Menyusun magnet tersebut adalah dengan cara meletakkannya di meja kemudian disambung dengan magnet berikutnya, apabila magnet tersebut saling Tarik-menarik maka magnet tersebut akan menjadi 1 grup, tetapi apabila saling tolak menolak, maka grup sebelumnya akan berhenti disusun dan ditinggal lalu grup berikutnya inilah yang akan dilanjutkan disusun. (pokoknya tinggal cek aja berapa kali magnet tersebut tolak-menolak dan kita akan dapat berapa banyak grup yang terbentuk)
+Program ini adalah program yang dibuat untuk menyelesaikan soal Codeforces yang berjudul MAGNETS dimana user ditugaskan
+membantu untuk menentukan berapa banyak grup magnet yang akan terbentuk dari suatu susunan magnet. Cara Menyusun magnet
+tersebut adalah dengan cara meletakkannya di meja kemudian disambung dengan magnet berikutnya, apabila magnet tersebut 
+saling Tarik-menarik maka magnet tersebut akan menjadi 1 grup, tetapi apabila saling tolak menolak, maka grup sebelumnya
+akan berhenti disusun dan ditinggal lalu grup berikutnya inilah yang akan dilanjutkan disusun. (pokoknya tinggal cek aja
+berapa kali magnet tersebut tolak-menolak dan kita akan dapat berapa banyak grup yang terbentuk)
+
 • Input
 n = banyak magnet, I tiap n = posisi terakhir magnet tersebut (positif atau negatife)
+
 • Output
 Banyak grup yang terbentuk
+
 • Source Code
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -173,9 +208,14 @@ int main(){
 
 76. LUCKY DIVISION • Source Code
  • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal CODEFORCES yang berjudul LUCKY DIVISION dimana user diminta untuk menentukan apakah sebuah angka adalah angka yang “almost lucky” atau hampir beruntung. Apakah angka “almost lucky” itu? Mereka adalah angka yang bisa dibagi tanpa bersisa oleh angka “lucky” atau beruntung. Apakah angka “lucky” itu? Mereka adalah angka yang mengandung 4, 7 atau keduanya. (Seperti 4, 7, 47, 477, dsb)
+Program ini adalah program yang dibuat untuk menyelesaikan soal CODEFORCES yang berjudul LUCKY DIVISION dimana user 
+diminta untuk menentukan apakah sebuah angka adalah angka yang “almost lucky” atau hampir beruntung. Apakah angka 
+“almost lucky” itu? Mereka adalah angka yang bisa dibagi tanpa bersisa oleh angka “lucky” atau beruntung. 
+ Apakah angka “lucky” itu? Mereka adalah angka yang mengandung 4, 7 atau keduanya. (Seperti 4, 7, 47, 477, dsb)
+
 • Input
 X = angka yang akan dicari tahu
+
 • Output
 Ya atau tidak
 Source Code:
@@ -201,11 +241,17 @@ return 0;
 
 77. WORD CAPITALIZATION • Source Code
 • Penjelasan
-Program ini adalah program yang dibuat untuk menyelesaikan soal CODEFORCES yang berjudul WORD CAPITALIZATION dimana user diminta untuk memeriksa huruf pertama saja dari sebuah kalimat, dan membuatnya menjadi huruf besar jika dia masih huruf kecil dan tidak diapa-apakan jika sudah berhuruf besar. (Meskipun nanti di tengah kata ada huruf besar, tidak perlu diapa-apakan, huruf pertama saja yang penting)
-• Input
+Program ini adalah program yang dibuat untuk menyelesaikan soal CODEFORCES yang berjudul WORD CAPITALIZATION dimana user 
+diminta untuk memeriksa huruf pertama saja dari sebuah kalimat, dan membuatnya menjadi huruf besar jika dia masih huruf 
+kecil dan tidak diapa-apakan jika sudah berhuruf besar. (Meskipun nanti di tengah kata ada huruf besar, tidak perlu 
+diapa-apakan, huruf pertama saja yang penting)
+
+ • Input
 X = sebuah kata
+
 • Output
 Kapitalisasi kata tersebut
+
 Source Code:
 #include<stdio.h> 
 #include<string.h> 
@@ -233,11 +279,18 @@ int main(){
 
 78. QUICKSORT • Source Code
 • Penjelasan
-Program ini adalah program yang dibuat untuk mengimplementasikan algoritma Quicksort, dimana algoritma ini dibuat untuk mengurutkan data dari terkecil sampai terbesar. Algoritma ini bekerja dengan cara memilih 1 elemen terserah sebagai elemen pivot dan kemudian mengelompokkan elemen yang lain menjadi 2 grup, yaitu lebih kecil atau lebih besar dari elemen pivot tersebut. Lalu di 2 grup ini akan dijalankan Kembali algoritma quicksort secara rekursif sampai akhirnya semua elemen pivot yang terpilih menjadi urut.
+Program ini adalah program yang dibuat untuk mengimplementasikan algoritma Quicksort, dimana algoritma ini dibuat 
+untuk mengurutkan data dari terkecil sampai terbesar. Algoritma ini bekerja dengan cara memilih 1 elemen terserah sebagai 
+elemen pivot dan kemudian mengelompokkan elemen yang lain menjadi 2 grup, yaitu lebih kecil atau lebih besar dari 
+elemen pivot tersebut. Lalu di 2 grup ini akan dijalankan Kembali algoritma quicksort secara rekursif sampai akhirnya 
+semua elemen pivot yang terpilih menjadi urut.
+
 • Input
 n = banyak angka, x tiap n = angka yang akan diurutkan
+
 • Output
 Angka setelah diurutkan
+
 Source Code:
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
@@ -276,11 +329,17 @@ int main(){
 
 79. COUNTING SORT • Source Code
 • Penjelasan
-Program ini adalah program yang dibuat untuk mengimplementasikan algoritma Counting Sort, dimana algoritma ini dibuat untuk mengurutkan data dari terkecil sampai terbesar. Algoritma ini bekerja dengan cara menghitung berapa banyak elemen integer yang berbeda dari suatu range integer, kemudian berdasarkan perhitungan ini kita bisa mengetahui posisi dari tiap angka yang tadi kita hitung
+Program ini adalah program yang dibuat untuk mengimplementasikan algoritma Counting Sort, dimana algoritma ini dibuat 
+untuk mengurutkan data dari terkecil sampai terbesar. Algoritma ini bekerja dengan cara menghitung berapa banyak elemen 
+integer yang berbeda dari suatu range integer, kemudian berdasarkan perhitungan ini kita bisa mengetahui posisi dari tiap 
+angka yang tadi kita hitung
+
 • Input
 n = banyak angka, x tiap n = angka yang akan diurutkan
+
 • Output
 Angka setelah diurutkan
+
 Source Code:
 #include<stdio.h> 
 #include<string.h> 
@@ -306,12 +365,18 @@ int main(){
 
 80. BUBBLE SWAP • Source Code
 • Penjelasan
-Program ini adalah program yang dibuat untuk mengimplementasikan Bubble Sort, dimana algoritma ini dibuat untuk mengurutkan data. Algoritma ini bekerja dengan cara membandingkan anggota ke 1 dengan anggota ke 2 dari sebiah array sepanjang n, jika anggota berikutnya lebih besar / kecil (tergantung kemauan) maka dilakukan penukaran. Lalu lanjut ke anggota berikutnya lagi, terus sampai habis. Lalu dilakukan pengulangan lagi dari anggota ke dua, dan seterusnya sampai urut. (sama seperti sorting di harga sepeda Tugas PATP)
+Program ini adalah program yang dibuat untuk mengimplementasikan Bubble Sort, dimana algoritma ini dibuat untuk mengurutkan 
+data. Algoritma ini bekerja dengan cara membandingkan anggota ke 1 dengan anggota ke 2 dari sebiah array sepanjang n, 
+jika anggota berikutnya lebih besar / kecil (tergantung kemauan) maka dilakukan penukaran. Lalu lanjut ke anggota berikutnya 
+lagi, terus sampai habis. Lalu dilakukan pengulangan lagi dari anggota ke dua, dan seterusnya sampai urut. 
+(sama seperti sorting di harga sepeda Tugas PATP)
 
 • Input
 n = banyak angka, x tiap n = angka yang akan diurutkan
+
 • Output
 Angka setelah diurutkan
+
 Source Code:
 #include<stdio.h> 
 //author : Evelyn Tjitrodjojo 
