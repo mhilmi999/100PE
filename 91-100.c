af6408fc9b489cf3274ff69f5e0b0ca9d605b1b0
mhilmi999 <54462717+mhilmi999@users.noreply.github.com>
2020-05-20 20:46:36 +0700

    Update 91-100.c

--- 91-100.c
+++ 91-100.c
@@ -368,28 +368,23 @@ e.	Selanjutnya untuk permutasi dengan rumus n P k = 5 P 5 = 5!/(5-5)! = 120.
 •	Codes :
 #include<stdio.h>
 //author : Evelyn Tjitrodjojo
-//translator : M. Hilmi Ramadhan
-
 int faktor(int);
-
 int main()
 {
     int n;
     scanf("%d",&n); //input angka faktorial 
     printf("%d\n",faktor(n)); //print hasil fungsi faktorial
 }
-int faktor(int a)
-{ //fungsi rekursif faktorial
+int faktor(int a){ //fungsi rekursif faktorial
     if(a==1) return 1; //jika angka=1, langsung print 1 
-    if(a%2==0)
-    if(a % 2 == 0)
-    {
+    if(a%2==0){
         a=(a/2)*faktor(a-1); //bila angka genap maka dikali fungsi faktor
     }
-    else 
-    {a*=faktor(a-1);} //jika angka dikali angka sebelumnya
-    
-    return a;
+    else
+    {
+        a*=faktor(a-1); //jika angka dikali angka sebelumnya
+        return a;
+    }
 }
 
 •	Penjelasan : 
